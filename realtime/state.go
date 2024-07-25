487e836efb4faae01796d94092e8dad89db0db58
Edward Coristine <edward.coristine@neuralink.com>
2024-07-25 00:46:38 -0700

    much simpler way to do this

--- realtime/state.go
+++ realtime/state.go
@@ -12,19 +12,14 @@ import (
 type Latency *int64
 
 var (
-	LatencyAverage     = make(map[*ipnstate.PeerStatus][]Latency)
+	LastLatencyResult  = make(map[*ipnstate.PeerStatus]Latency)
 	LatencyTestLimiter = rate.NewLimiter(1, 1)
 )
 
 func GetExitNodeRtt(nodes []*ipnstate.PeerStatus) (map[*ipnstate.PeerStatus]Latency, error) {
-	for node, lastTenTries := range LatencyAverage {
-		if len(lastTenTries) > 0 {
-			// kick out the last entry
-			LatencyAverage[node] = lastTenTries[1:]
-		}
-	}
-
 	if LatencyTestLimiter.Allow() {
+		latencies := make(map[*ipnstate.PeerStatus]Latency)
+
 		if pinger, err := ping.New("0.0.0.0", ""); err == nil {
 			for _, node := range nodes {
 				// the first address is the ipv4 one
@@ -39,32 +34,12 @@ func GetExitNodeRtt(nodes []*ipnstate.PeerStatus) (map[*ipnstate.PeerStatus]Late
 
 				ms := rtt.Milliseconds()
 
-				if nodeAverage := LatencyAverage[node]; len(nodeAverage) > 0 {
-					LatencyAverage[node] = append(nodeAverage, &ms)
-				} else {
-					LatencyAverage[node] = []Latency{&ms}
-				}
+				latencies[node] = &ms
 			}
 		}
+
+		LastLatencyResult = latencies
 	}
 
-	latency := make(map[*ipnstate.PeerStatus]Latency)
-
-	for node, lastTenTries := range LatencyAverage {
-		sum := int64(0)
-
-		for _, m := range lastTenTries {
-			sum += *m
-		}
-
-		if len(lastTenTries) != 0 {
-			nodeLatency := sum / int64(len(lastTenTries))
-
-			latency[node] = &nodeLatency
-		} else {
-			latency[node] = nil
-		}
-	}
-
-	return latency, nil
+	return LastLatencyResult, nil
 }
